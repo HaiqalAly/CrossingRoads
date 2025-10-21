09e0ac4f831437075c74b966146ce22970f815cb
Haiqal <haiqalaly@gmail.com>
2025-10-21 20:25:05 +0700

    Adding car spawner
    Adding car spawner using signal timer and marker

--- script/game.gd
+++ script/game.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var car_scene: PackedScene = preload("res://scenes/car.tscn")
 
-func _on_timer_timeout() -> void:
-	print('Timer ready')
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("Area entered by: ", body.name)
+
+
+func _on_car_timer_timeout() -> void:
+	var car = car_scene.instantiate() as Area2D
+	var pos_marker = $CarStartPosition.get_children().pick_random()
+	car.position = pos_marker.position
+	$Object.add_child(car)
